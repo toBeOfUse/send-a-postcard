781ced979c961395d0fa2df0f6c7c5c9121f89bc
toBeOfUse <mitchjacov@gmail.com>
2021-12-20 20:53:54 +0000

    add send button and very simple backend

---
+++ back.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const fs = require("fs");
+
+if (!fs.existsSync("index.txt")){
+    fs.writeFileSync("index.txt", "");
+}
+
+http.createServer((req, res)=>{
+    req.setEncoding("utf-8");
+    let body = "";
+    if (req.method=="POST" && req.url == "/mail/box"){
+        const timestamp = new Date().toLocaleString();
+        fs.appendFileSync("inbox.txt", "Receiving message at "+timestamp+":\n");
+        req.on("data", (c)=>{
+            console.log("received", c);
+            body += c;
+        });
+        req.on("end", () => {
+            fs.appendFile("inbox.txt", body+"\n---\n",
+            function (err) {
+                if (err) throw err;
+                console.log('appended message to inbox.txt');
+            });
+            res.statusCode = 200;
+            res.end();
+        })
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
+}).listen(8090);
+console.log("server listening on port 8090");
