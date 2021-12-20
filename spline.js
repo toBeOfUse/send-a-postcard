38ecc41d559daa17f574b19c8016ee2a6449cb7c
toBeOfUse <mitchjacov@gmail.com>
2021-12-20 22:57:37 +0000

    develop animation code into a proper framework

--- spline.js
+++ spline.js
@@ -1,26 +1,85 @@
-function lerp(p1, p2, t){
-    return [p1[0]*(1-t)+p2[0]*(t), p1[1]*(1-t)+p2[1]*t];
+function lerp(v1, v2, t){
+    return v1*(1-t)+v2*t;
 }
 
-function spline(t){
-    let points = [[0,0], [0.15, 0.8], [0.8, 0.15], [1,1]];
+function pointLerp(p1, p2, t){
+    return [lerp(p1[0], p2[0], t), lerp(p1[1], p2[1], t)];
+}
+
+function spline(t, splinePoints){
+    let points = splinePoints;
     while (points.length>1){
         const iteration = [];
         for (let i=0; i<points.length-1;i++){
             const segment = points.slice(i, i+2);
-            iteration.push(lerp(segment[0], segment[1], t));
+            iteration.push(pointLerp(segment[0], segment[1], t));
         }
         points = iteration;
     }
     return points[0][1];
 }
 
-// function test(){
+class Keyframe {
+    constructor(time, value, spline){
+        this.time = time;
+        this.value = value;
+        this.spline = spline;
+    }
+}
+
+class Track {
+    constructor(keyframes){
+        this.keyframes = keyframes;
+    }
+    get start(){
+        return this.keyframes[0].time;
+    }
+    get length() {
+        return this.keyframes[this.keyframes.length-1].time-this.start;
+    }
+    get end(){
+        return this.start+this.length;
+    }
+    getValue(time){
+        if (time <= this.start){
+            return this.keyframes[0].value;
+        } else if (time >= this.end){
+            return this.keyframes[this.keyframes.length-1].value;
+        } else {
+            const afterIndex = this.keyframes.findIndex(v=>v.time>=time);
+            const after = this.keyframes[afterIndex];
+            if (after.time == time){
+                return after.value;
+            }
+            const before = this.keyframes[afterIndex-1];
+            const weight = (time-before.time)/(after.time-before.time);
+            if (after.spline){
+                const metaWeight = spline(weight, after.spline);
+                return lerp(before.value, after.value, metaWeight);
+            } else {
+                return lerp(before.value, after.value, weight);
+            }
+        }
+    }
+}
+
+class Animation {
+    constructor(rx, ry, rz, tx, ty, tz){
+        this.rx = rx;
+        this.ry = ry;
+        this.rz = rz;
+        this.tx = tx;
+        this.ty = ty;
+        this.tz = tz;
+    }
+}
+
+// function testSpline(){
 //     for (let j=0; j<=1; j+=0.1){
 //         console.log(spline(j));
 //     }
 // }
 
-// test();
+// testSpline();
 
-export default spline;
+export {Animation, Track, Keyframe};
